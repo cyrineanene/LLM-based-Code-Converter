081cafdb4163d0295733ac5c0c6276583a8241cd
cyrineanene <cyrine.anene@supcom.tn>
2024-11-02 09:40:39 +0100

    updating branch structure

--- main.py
+++ main.py
@@ -1,16 +1,1 @@
-from Description.generate_description import Description
-
-code_snippet = '''
-def calculate_area(length, width):
-    return length * width
-
-length = float(input("Enter the length of the rectangle: "))
-width = float(input("Enter the width of the rectangle: "))
-area = calculate_area(length, width)
-print(f"The area of the rectangle is: {area}")
-    '''
-
-description = Description(code_snippet)
-des = description.get_code_description()
-print("Description of the code:")
-print(description.output(des))
+from Seperation.prompt_seperation import Description
