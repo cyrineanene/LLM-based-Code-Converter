1cde3984ab6e27057b43ea22f3bd4277f55a2653
cyrineanene <cyrine.anene@supcom.tn>
2025-01-17 15:53:15 +0100

    done working on the assembler

--- src/assembling.py
+++ src/assembling.py
@@ -1,3 +1,13 @@
 class Assembler: 
-    def __init__(self, code):
-        self.code = code
+    def __init__(self, generated_code, file_name):
+        self.generated_code = generated_code
+        self.file_name = file_name
+    def assemble_java_code(self):
+        if not self.file_name.endswith(".java"):
+            self.file_name += ".java"
+        try:
+            with open(self.file_name, "a") as file:
+                file.write("\n" + self.generated_code)  
+                print(f"Java code appended to '{self.file_name}' successfully.")
+        except Exception as e:
+            print(f"An error occurred: {e}")
